9eeb367d9541019bbc5e59064237639e46df87a3
kappachino21 <113945588+kappachino21@users.noreply.github.com>
2022-10-11 06:51:20 +0800

    Add files via upload

---
+++ classwork.js
@@ -0,0 +1,29 @@
+let dwarf = "Bob";
+let age = 18;
+let bootcamp = true;
+
+let addTwo = 5 + 6;
+
+let twoStrings = "two" + "strings";
+
+let nameOne = "Daniel";
+let nameTwo = "Jason";
+let ageTwo = 20;
+
+if (nameOne === nameTwo && ageTwo === 20) {
+  console.log("Hello, Daniel");
+} else if (nameOne !== nameTwo) {
+  console.log("Hello, Bob");
+}
+
+let person = ["Jenny", 24, 150];
+
+let daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
